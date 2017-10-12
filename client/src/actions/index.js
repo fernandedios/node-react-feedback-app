a223314d9df8d1702a3e98b47a61cef2f1a651ab
Fernan de Dios <info@mediagramstudios.com>
2017-10-12 14:13:58 +0800

    Added authentication action creator

---
+++ client/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+// promise version
+/*export const fetchUser = () => {
+   return function(dispatch) {
+    axios.get('/api/current_user')
+      .then(res => dispatch({ type: FETCH_USER, payload: res }));
+  }
+}; */
+
+// async await version
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res });
+};
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions'; // get all action creators
 
 import Header from './Header';
 
@@ -7,21 +9,28 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>Survey New</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  // BrowserRouter accepts only ONE child component
-  // exact //- match the path exactly in order to show the component, === exact={true}
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    // check if user is authenticated
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    // BrowserRouter accepts only ONE child component
+    // exact //- match the path exactly in order to show the component, === exact={true}
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
