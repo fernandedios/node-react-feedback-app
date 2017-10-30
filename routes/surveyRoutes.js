bb9fe13d549237accb31c2b75f397b769f9ad90d
Fernan de Dios <info@mediagramstudios.com>
2017-10-30 13:46:38 +0800

    SendGrid initial setup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -4,19 +4,21 @@ const requireCredits = require('../middlewares/requireCredits');
 
 const Survey = mongoose.model('surveys'); // get model directly from mongoose
 
-module.exports = app = {
+module.exports = app => {
   // check if user is authenticated, check if user has enough credits
   // take special care of the middleware order
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) {
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
       title,
       subject,
       body,
-      recipients: recipients.split(',').map(email => ({ email: email.trim() })), // wrap object with parens to avoid errors, trim whitespaces
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })), // wrap object with parens to avoid errors, trim whitespaces on email
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+
   });
 };
