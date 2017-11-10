f771cc2258b0b2b47afe26b61a0a106a9273d9a9
Fernan de Dios <info@mediagramstudios.com>
2017-11-10 11:09:53 +0800

    Wire up survey list to dashboard

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 // promise version
 /*export const fetchUser = () => {
@@ -23,6 +23,12 @@ export const handleToken = (token) => async dispatch => {
 export const submitSurvey = (values, history) => async dispatch => {
   const res = await axios.post('/api/surveys', values);
 
-  history.push('/surveys'); // navigate to /surveys 
+  history.push('/surveys'); // navigate to /surveys
   dispatch({ type: FETCH_USER, payload: res.data }); // reuse FETCH_USER to update header
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form'; // set different variable name to better describe what it does
 
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm // set state key to reduxForm reducer
+  form: reduxForm, // set state key to reduxForm reducer
+  surveys: surveysReducer
 });
