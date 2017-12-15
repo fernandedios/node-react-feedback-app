1352aab8a15f7ced797fa80c013bd9f80f6dec37
Fernan de Dios <info@mediagramstudios.com>
2017-12-15 17:17:35 +0800

    add surveysamples component

---
+++ client/src/components/common/SurveySamples.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+const SurveySamples = () => {
+  return (
+    <section id="home-icons" className="py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="http://lorempixel.com/200/200/business/1" />
+              <div className="card-block text-center">
+                <h3 className="card-title">Customer Satisfaction Survey</h3>
+                <p className="card-text">Finding out how customers feel means knowing how to improve.</p>
+                <a href="#" className="btn card-btn btn-common">Learn More</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="http://lorempixel.com/200/200/business/2" />
+              <div className="card-block text-center">
+                <h3 className="card-title">Pre-Event Survey</h3>
+                <p className="card-text">This survey can be used in advance of an event to help with planning and preparation, including identifying location ...</p>
+                <a href="#" className="btn card-btn btn-common">Learn More</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="http://lorempixel.com/200/200/business/3" />
+              <div className="card-block text-center">
+                <h3 className="card-title">Engagement Survey</h3>
+                <p className="card-text">Ensuring high engagement among teachers promotes student success and a healthier and happier school community.</p>
+                <a href="#" className="btn card-btn btn-common">Learn More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="http://lorempixel.com/200/200/business/4" />
+              <div className="card-block text-center">
+                <h3 className="card-title">Professional Development Course</h3>
+                <p className="card-text">Following a professional development course, this form collects participant feedback regarding the instructor, the co...</p>
+                <a href="#" className="btn card-btn btn-common">Learn More</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="http://lorempixel.com/200/200/business/5" />
+              <div className="card-block text-center">
+                <h3 className="card-title">Product Feedback Survey</h3>
+                <p className="card-text">Make sure your development plans include customer feedback.</p>
+                <a href="#" className="btn card-btn btn-common">Learn More</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="http://lorempixel.com/200/200/business/6" />
+              <div className="card-block text-center">
+                <h3 className="card-title">First Time Visitor Survey</h3>
+                <p className="card-text">Collect feedback from individuals visiting a store. The main emphasis of the survey is to understand what their plan...</p>
+                <a href="#" className="btn card-btn btn-common">Learn More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SurveySamples;
