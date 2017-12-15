f3df4c08b19dc0dd1496c9e3b4bcde89a80df83a
Fernan de Dios <info@mediagramstudios.com>
2017-12-15 17:21:01 +0800

    add insight component

---
+++ client/src/components/common/Insight.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Insight = () => {
+  return (
+    <section id="home-heading" className="p-5">
+      <div className="dark-overlay">
+        <div className="row">
+          <div className="col">
+            <div className="container pt-5">
+              <h1>Survey Reports Designed For Insight</h1>
+              <p className="d-none d-md-block">SoGoSurvey’s reporting tool lets you create beautiful, insightful, real-time reports with ease. Choose from an array of options to analyze complex data and display the precise results you need in the customized perfection you’re looking for.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Insight;
