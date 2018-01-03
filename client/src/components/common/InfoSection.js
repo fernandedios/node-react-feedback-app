26584ac12c66272938872664b8fc95863ca59512
Fernan de Dios <info@mediagramstudios.com>
2018-01-03 17:37:50 +0800

    connect InfoSection component to redux

--- client/src/components/common/InfoSection.js
+++ client/src/components/common/InfoSection.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const InfoSection = () => {
+const InfoSection = ({ auth }) => {
   return (
     <section id="info" className="py-3">
       <div className="container">
@@ -9,7 +11,7 @@ const InfoSection = () => {
             <h3>Keep it simple. Survey.</h3>
             <p>Studies suggest over 80% of the worldwide workforce is disengaged. Disengaged employees lead to dissatisfied customers, churn leads to loss of
     revenue – the impact is staggering. Positive engagement helps your environment, innovation, growth – and your bottom line.</p>
-            <a href="/auth/google" className="btn std-btn btn-filled">Learn more</a>
+            <Link href={auth ? '/surveys' : '/auth/google'} className="btn std-btn btn-filled">{auth ? 'Dashboard' : 'Learn More'}</Link>
           </div>
           <div className="col-md-6">
             <img src="img/laptop.png" className="img-fluid" />
@@ -20,4 +22,8 @@ const InfoSection = () => {
   );
 };
 
-export default InfoSection;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(InfoSection);
