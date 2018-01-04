98afc75f4e0e1d9097db8f122979767ec984b950
Fernan de Dios <info@mediagramstudios.com>
2018-01-04 14:06:29 +0800

    react-router-dom bug fixes

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import SurveyList from './surveys/SurveyList';
 import SurveySamples from './common/SurveySamples';
--- client/src/components/common/InfoSection.js
+++ client/src/components/common/InfoSection.js
@@ -11,7 +11,7 @@ const InfoSection = ({ auth }) => {
             <h3>Keep it simple. Survey.</h3>
             <p>Studies suggest over 80% of the worldwide workforce is disengaged. Disengaged employees lead to dissatisfied customers, churn leads to loss of
     revenue – the impact is staggering. Positive engagement helps your environment, innovation, growth – and your bottom line.</p>
-            <Link href={auth ? '/surveys' : '/auth/google'} className="btn std-btn btn-filled">{auth ? 'Dashboard' : 'Learn More'}</Link>
+            <Link to={auth ? '/surveys' : '/auth/google'} className="btn std-btn btn-filled">{auth ? 'Dashboard' : 'Learn More'}</Link>
           </div>
           <div className="col-md-6">
             <img src="img/laptop.png" className="img-fluid" />
