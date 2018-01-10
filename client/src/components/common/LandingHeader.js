5ef6cd86e19b412e38100973dbd4d09494822725
Fernan de Dios <info@mediagramstudios.com>
2018-01-10 14:57:05 +0800

    convert LandingHeader to class component

--- client/src/components/common/LandingHeader.js
+++ client/src/components/common/LandingHeader.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const LandingHeader = ({ auth }) => {
-  return (
-    <div className="header-style-3">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="pt-3" id="header-home">
-              <h1>{auth ? 'Access your surveys' : 'Create your online survey now!'}</h1>
-              <p>Get the data you need to make better decisions.</p>
-              <p><Link to={auth ? '/surveys' : '/auth/google'} className="btn std-btn btn-filled">{auth ? 'Dashboard' : 'Learn More'}</Link></p>
+class LandingHeader extends Component {
+  renderContent() {
+    return (
+      <div className="pt-3" id="header-home">
+        <h1>{this.props.auth ? 'Access your surveys' : 'Create your online survey now!'}</h1>
+        <p>Get the data you need to make better decisions.</p>
+        <p><Link to={this.props.auth ? '/surveys' : '/auth/google'} className="btn std-btn btn-filled">{this.props.auth ? 'Dashboard' : 'Learn More'}</Link></p>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="header-style-3">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              {this.renderContent()}
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 function mapStateToProps({ auth }) {
   return { auth };
