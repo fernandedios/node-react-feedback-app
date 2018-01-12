843fe7608783b2d55a55ab9a73eb234bbe4c47d9
Fernan de Dios <info@mediagramstudios.com>
2018-01-12 18:03:53 +0800

    restyle form review page

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -10,30 +10,46 @@ import * as actions from '../../actions';
 // submitSurvey prop passed as action creator
 // history prop is passed from withRouter helper
 const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
-  const reviewFields = _.map(formFields, ({ label, name }) => {
+  const reviewFields = _.map(formFields, ({ label, name, size }) => {
     return (
-      <div key={name}>
-        <label>{label}</label>
-        <div>{formValues[name]}</div>
+      <div className={`col-md-${size} mb-2`} key={name}>
+        <div className="blog-item-wrapper">
+          <div className="blog-item-text">
+            <label><b>{label}</b></label>
+            <p>{formValues[name]}</p>
+          </div>
+        </div>
       </div>
     );
   });
 
   // wrap submitSurvey in an arrow function so it is NOT invoked automatically when component is first rendered
   // pass history object to submitSurvey action creator
-  // TODO: add loading animation when sending survey 
+  // TODO: add loading animation when sending survey
   return (
-    <div>
-      <h5>Please confirm your entries</h5>
-      {reviewFields}
-      <button className="yellow darken-3white-text btn-flat" onClick={onCancel}>
-        Back
-      </button>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="page-header-title">
+            <h2 className="heading-title text-center">Please confirm your entries</h2>
+          </div>
+        </div>
+      </div>
 
-      <button className="green btn-flat white-text right" onClick={() => { submitSurvey(formValues, history) }}>
-        Send Survey
-        <i className="material-icons right">email</i>
-      </button>
+      <div className="row">
+        {reviewFields}
+
+        <div className="col-md-6">
+          <button className="float-right btn std-btn btn-common" onClick={onCancel}>
+            Back
+          </button>
+        </div>
+        <div className="col-md-6">
+          <button class="btn std-btn btn-info-filled" onClick={() => { submitSurvey(formValues, history) }}>
+            Send Survey <i className="fa fa-envelope" aria-hidden="true"></i>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
